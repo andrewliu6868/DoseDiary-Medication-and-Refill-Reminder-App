4d0f58bf07c9ab9cde0ba11c5173fc7eabf8770e
LOVISH CHUGH <lchugh@uwaterloo.ca>
2024-06-22 17:05:40 -0400

    Added import for EditMedication

--- app/src/main/java/com/example/dosediary/view/MedicationHistory.kt
+++ app/src/main/java/com/example/dosediary/view/MedicationHistory.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import com.example.DoseDiary.EditMedication
 
 
 @Composable
