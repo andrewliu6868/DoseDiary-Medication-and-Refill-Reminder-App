ed7e085fa5510def4ba71a7f4932bacae5902ab9
andaniel01 <d22an@uwaterloo.ca>
2024-07-10 16:27:17 -0400

    google maps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
