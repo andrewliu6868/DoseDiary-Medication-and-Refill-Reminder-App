91dccae145a6255cebb553e49f447747918fff4a
Brian Wu <byhwu@uwaterloo.ca>
2024-07-15 23:39:27 -0400

    Refactor DatePicker and TimePicker

--- app/src/main/java/com/example/dosediary/view/HomePage.kt
+++ app/src/main/java/com/example/dosediary/view/HomePage.kt
@@ -28,7 +28,6 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Date
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MedicationListScreen(navController: NavController, medRefillViewModel: MedRefillViewModel) {
 
