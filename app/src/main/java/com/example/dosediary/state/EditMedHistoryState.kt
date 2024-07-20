5103d0c4287947a54a87bb0b5d779ff6ff76cef6
LOVISH CHUGH <lchugh@uwaterloo.ca>
2024-07-20 14:54:37 -0400

    EditMedHistory State

---
+++ app/src/main/java/com/example/dosediary/state/EditMedHistoryState.kt
@@ -0,0 +1,10 @@
+package com.example.dosediary.state
+
+import com.example.dosediary.model.entity.MedicationHistory
+
+data class EditMedHistoryState (
+    val medicationHistories: List<MedicationHistory> = emptyList(),
+    val selectedMedicationHistory: MedicationHistory? = null,
+    val effectiveness: String = "",
+    val showConfirmDialog: Boolean = false
+)
